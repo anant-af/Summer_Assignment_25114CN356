320c3fda43e84103ae458de2619fb5122b0b67af
anant-af <af.anantk@gmail.com>
2026-06-01 03:16:10 +0530

    Day_1 Uploaded

---
+++ Day_1/Q1.c
@@ -0,0 +1,20 @@
+// WAP to calculate sum of first N natural numbers
+
+
+#include<stdio.h>
+int main () {
+    int i,n,num ;
+    printf("enter natural no.:");
+    scanf("%d",&n);
+
+    num=0;
+    for(i=1;i<=n;i++)
+    {
+        num = num + i ;
+    }
+    printf("Sum of first %d natural no.: %d",n,num);
+
+    return 0;
+    
+}  
+
